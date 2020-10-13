1326b500b019b312a69823126e210ab92b24c994
Shaik Mohsin Mohammad <skmohsinmohd@gmail.com>
2020-10-13 22:35:22 +0530

    added required logic to handle CRUD operations

--- src/controllers/meetings-controller.js
+++ src/controllers/meetings-controller.js
@@ -1,25 +1,26 @@
 const meetingDao = require('../daos/meeting-dao');
 
-module.exports.getAllmeetings = () => {
-    meetingDao.getAllmeetings();
+module.exports.getAllmeetings = (userId) => {
+    meetingDao.getAllmeetings(userId);
 };
 
 module.exports.getMeeting = (id) => {
-    meetingDao.getAllmeetings();
+    meetingDao.getMeeting(id);
 };
 
 module.exports.createMeeting = (req) => {
-    const meeting = { meetingName: req.meetingname, Email: req.email, Password: req.password};
-    meetingDao.signUp(meeting);
+    const meeting = { Title: req.title, MeetingDate: req.meetingDate, UserId: req.userId};
+    const attendees = req.attendees;
+    meetingDao.createMeeting(meeting, attendees);
 }
 
-module.exports.deleteMeeting = (req) => {
-    const meeting = { meetingName: req.meetingname, Email: req.email, Password: req.password};
-    meetingDao.signUp(meeting);
+module.exports.deleteMeeting = (id) => {
+    meetingDao.deleteMeeting(id);
 }
 
 module.exports.updateMeeting = (req) => {
-    const meeting = { meetingName: req.meetingname, Email: req.email, Password: req.password};
-    meetingDao.signUp(meeting);
+    const meeting = { MeetingId: req.meetingId, Title: req.title, MeetingDate: req.meetingDate, UserId: req.userId};
+    const attendees = req.attendees;
+    meetingDao.updateMeeting(meeting, attendees);
 }
 
--- src/daos/meeting-dao.js
+++ src/daos/meeting-dao.js
@@ -8,34 +8,73 @@ const getAllMeetings = () => {
         console.log(rows);
     })
 };
-const createMeeting = (Meeting) => {    
+
+const createMeeting = (meeting, attendees) => {    
     con.dbConnection.query('INSERT INTO Meetings SET ?', Meeting, (err, res) => {
         if(err) throw err;
         console.log('Last insert ID:', res.insertId);
+        
+        let toBeSavedAttendees = [];
+        attendees.forEach(attendee => {
+            toBeSavedAttendees.push({ meetingID: res.insertId, email: attendee });
+        });
+        console.log(toBeSavedAttendees);
+
+        con.dbConnection.query('INSERT INTO Attendees VALUES ?', [toBeSavedAttendees], (err, res) => {
+            if(err) throw err;
+            console.log('Attendees inserted');
+        })
     })
 };
 
 const updateMeeting = (Meeting) => {    
-    con.dbConnection.query('INSERT INTO Meetings SET ?', Meeting, (err, res) => {
+    con.dbConnection.query(
+        'UPDATE Meetings SET Title ? MeetingDate ? WHERE MeetingId ?', [meeting.title, meeting.meetingDate, meeting.meetingID],
+        (err, result) => {
+            if (err) throw err;
+        
+            console.log(`Changed ${result.changedRows} row(s)`);
+            con.dbConnection.query(
+                'DELETE FROM ATTENDEES WHERE MeetingId ?',  meeting.meetingID,
+                (err, result) => {
+                if (err) throw err;
+                console.log("Delete Attendees");
+                }
+            )
+            let toBeSavedAttendees = [];
+            attendees.forEach(attendee => {
+                toBeSavedAttendees.push({ meetingID: res.insertId, email: attendee });
+            });
+            console.log(toBeSavedAttendees);
+
+            con.dbConnection.query('INSERT INTO Attendees VALUES ?', [toBeSavedAttendees], (err, res) => {
+                if(err) throw err;
+                console.log('Attendees inserted');
+            })
+        }
+      );
+};
+
+
+const deleteMeeting = (id) => {
+    con.dbConnection.query(
+        'DELETE FROM Meetings WHERE MeetingId = ?', id, (err, result) => {
+        if (err) throw err;
+      
+        console.log(`Deleted ${result.affectedRows} row(s)`);
+        }
+    );
+};
+
+const getMeeting = (id) => {    
+    con.dbConnection.query(
+        'SELECT * FROM Meetings WHERE MeetingId = ?', id, (err, res) => {
         if(err) throw err;
-        console.log('Last insert ID:', res.insertId);
+        console.log('Update meeting');
     })
 };
 
 
-const deleteMeeting = (Meeting) => {    
-    con.dbConnection.query('INSERT INTO Meetings SET ?', Meeting, (err, res) => {
-        if(err) throw err;
-        console.log('Last insert ID:', res.insertId);
-    })
-};
-
-const getMeeting = (Meeting) => {    
-    con.dbConnection.query('INSERT INTO Meetings SET ?', Meeting, (err, res) => {
-        if(err) throw err;
-        console.log('Last insert ID:', res.insertId);
-    })
-};
 
 module.exports.getAllMeetings = getAllMeetings;
 module.exports.getMeeting = getMeeting;
